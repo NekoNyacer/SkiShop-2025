48678cd339643e8b3b1d7478854956718babbe94
EddieLiu <mariomario258@hotmail.com.tw>
2026-01-03 23:41:28 +0800

    end of section 6

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,38 @@
+using API.DTOs;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    [HttpGet("unauthorized")]
+    public IActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    }
+
+    [HttpGet("badrequest")]
+    public IActionResult GetBadRequest()
+    {
+        return BadRequest("回應怪怪的");
+    }
+    [HttpGet("notfound")]
+    public IActionResult GetNotFound()
+    {
+        return NotFound();
+    }
+
+    [HttpGet("internalerror")]
+    public IActionResult GetInternalError()
+    {
+        throw new Exception("測試錯誤例外");
+    }
+
+    [HttpPost("validationerror")]
+    public IActionResult GetValidationError(CreateProductDto product)
+    {
+        return Ok();
+    }
+
+}
